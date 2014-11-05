a1138e633068ae75d49b42409a859f8feda88cbc
Arie <awprogramming@gmail.com>
2014-11-05 14:18:24 -0500

    Basic JSON object created

--- Twitter/test.php
+++ Twitter/test.php
@@ -9,37 +9,96 @@ $settings = array(
 'consumer_secret' => "SlROjgVpdrfkLUvm04LNxP7jTmI0EzTHxcoZxfowxE9CmdTzBh"
 );
 
+$user = 'emilysimms7';
+$twitterInfo = array();
+
 $twitter = new TwitterAPIExchange($settings);
 
-/** Perform a GET request and echo the response **/
-/** Note: Set the GET field BEFORE calling buildOauth(); **/
-$url = 'https://api.twitter.com/1.1/followers/ids.json';
-$getfield = '?screen_name=Gorelick92&count=10'; //**** NAME HERE ****
-$requestMethod = 'GET';
+$followerIDs = getIDs($twitter,$user);
+$twitterInfo['follower_count'] = count($followerIDs);
+//echo "FOLLOWER COUNT: " .  count($followerIDs);
+//echo "</br></br>";
+$twitterInfo['follower_ids'] = $followerIDs;
+// echo "FOLLOWER LIST: " . json_encode($followerIDs);
+// echo "</br></br>";
 
-
-//echo $twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest();
-
-$decoded = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest());
-
-$ids = $decoded->ids;
-
-$next_cursor = $decoded->next_cursor;
-
-while($next_cursor != 0)
+//echo "LOCATIONS: </br></br>";
+$followers = array();
+for($i=0;$i<14;$i++)
 {
-	$url = 'https://api.twitter.com/1.1/followers/ids.json';
-	$getfield = '?screen_name=Gorelick92&cursor=' . $next_cursor; //**** NAME HERE ****
+	if(is_null($followerIDs[$i]))
+	{
+		break;
+	}
+	$url = 'https://api.twitter.com/1.1/users/show.json';
+	$getfield = '?user_id=' . $followerIDs[$i];
+	$requestMethod = 'GET';
+	$decoded = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest());
+	$location = $decoded->location;
+
+	if($location == null)
+	{
+		$location = "No Location Listed";
+	}
+	
+	// echo "<b>ID: " . $followerIDs[$i] . "</b></br>";
+	// echo $location . "</br>";
+
+	// getIDs($twitter,$user,$followerIDs[$i]) . "</br></br>";
+	$followers[] = array(
+			'id' => $followerIDs[$i],
+			'location' => $location,
+			'following' => getIDs($twitter,$user,$followerIDs[$i])
+		);
+}
+
+$twitterInfo['followers'] = $followers;
+
+echo json_encode($twitterInfo);
+
+function getIDs($twitter,$twitterSN,$twitterID=null)
+{
+	$whichType = "followers";
+	$getfieldPiece = '?screen_name=' . $twitterSN;
+	if(!is_null($twitterID))
+	{
+		$whichType = "friends";
+		$getfieldPiece = '?user_id=' . $twitterID;
+	}
+
+	$url = 'https://api.twitter.com/1.1/'. $whichType .'/ids.json';
+	$getfield = $getfieldPiece; 
 	$requestMethod = 'GET';
 
 	$decoded = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest());
-	$moreids = $decoded->ids;
 
-	$ids = array_merge($ids,$moreids);
+	if(!is_null($decoded->errors))
+	{
+		return $decoded->errors[0]->message;
+	}
 
+	$ids = $decoded->ids;
+
+	$idsForCount = $ids;
+
+	// CODE TO CURSOR THROUGH ALL FOLLOWERS
 	$next_cursor = $decoded->next_cursor;
-}
 
-//$ids has all ids!
+	while($next_cursor != 0)
+	{
+		$url = 'https://api.twitter.com/1.1/'.$whichType.'/ids.json';
+		$getfield = $getfieldPiece . '&cursor=' . $next_cursor; //**** NAME HERE ****
+		$requestMethod = 'GET';
+
+		$decoded = json_decode($twitter->setGetfield($getfield)->buildOauth($url, $requestMethod)->performRequest());
+		$moreids = $decoded->ids;
+
+		$idsForCount = array_merge($idsForCount,$moreids);
+
+		$next_cursor = $decoded->next_cursor;
+	}
+
+	return $idsForCount;
+}
 
 
